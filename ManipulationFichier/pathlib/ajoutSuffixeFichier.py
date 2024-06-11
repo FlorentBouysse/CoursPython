c3c13dd5a1138418c7a2a735a1ceb68070459817
Bouysse <florent.bouysse@oclock.school>
2024-06-11 09:29:24 +0200

    Add text

---
+++ ManipulationFichier/pathlib/ajoutSuffixeFichier.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+p = Path.cwd() / "ManipulationFichier" / "pathlib" / "img.png";
+
+# p.touch();
+
+p = p.parent / (p.stem + "-lowres" + p.suffix)
+
+p.touch();
+print(p);
+
